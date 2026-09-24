224f9615d8f4c274b717c41ce1e0fb19c7381a37
Eirin <eiringonzales@gmail.com>
2026-09-24 18:15:46 +0800

    EDIT: footer content and font styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,21 +8,17 @@ const Footer = () => {
     <footer className="wide-container">
       <div className="row">
         <div>
-          <h4>Menu</h4>
+          <h4>Developer</h4>
+          <p>Eirin Gonzales</p>
           <p><a href={assetUrl('assets/eiringonzales_resume.pdf')} target="_blank" rel="noopener noreferrer">See my Resume</a></p>
           {/* <p><Link to="/portfolio">Look at my Portfolio</Link></p> */}
           <p><a href="https://ph.linkedin.com/in/eirin-gonzales-5951aa9b" aria-label="Linkedin" target="_blank" rel="noopener noreferrer">Connect with me on LinkedIn</a></p>
         </div>
         <div>
           <h4>Tech Tools Used Here</h4>
-          <p>Front End: HTML, CSS, React</p>
+          <p>Front End: Vite, HTML, CSS, React</p>
           <p>Assets: Google Fonts, Unsplash.com, Font Awesome Icons</p>
           <p>Hosting: GitHub Pages</p>
-        </div>
-        <div>
-          <h4>Developer</h4>
-          <p>Eirin Gonzales</p>
-          <p>Check out <a href="https://www.freecodecamp.org/">Free Code Camp</a> if you want to start learning about web development.</p>
         </div>
       </div>
       <div className="footer-copy">&copy; {new Date().getFullYear()} Eirin Gonzales</div>
